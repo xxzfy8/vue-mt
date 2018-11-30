d3dd0e3dd5dac9a4a3375285a0e1fb99ce9a6b28
张方洋 <xxzfy8@126.com>
2018-11-30 18:08:12 +0800

    update

--- koa2-learn/middleware/koa-pv.js
+++ koa2-learn/middleware/koa-pv.js
@@ -2,5 +2,10 @@ function pv(ctx) {
     // 进入app中会有一个对象，这个对象会挂载所有的信息
     // 1、request 信息  2、response 信息
     global.console.log(ctx.path)
-    
+
+}
+module.exports = fucntion() {
+    return async function(ctx, next) {
+        pv(ctx)
+    }
 }
